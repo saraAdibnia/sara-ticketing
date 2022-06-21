edd70c8122eacee69a8fe09e39a2112d45ba07a8
ruhy <ruhollahmozafari@gmail.com>
2022-06-21 07:47:43 +0000

    fix blender in swagger

--- TicketingSystem/urls.py
+++ TicketingSystem/urls.py
@@ -38,7 +38,11 @@ urlpatterns = [
 
 # swagger added by ruhy 
 urlpatterns += [
+    # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
     path('swagger/',
          SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/',
+         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
